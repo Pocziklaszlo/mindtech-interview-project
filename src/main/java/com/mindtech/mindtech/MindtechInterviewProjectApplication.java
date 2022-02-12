3b8e20e770eeeb9f058004c4623465ddaf22c2d2
Pocziklaszlo <laszlo.poczik@yahoo.com>
2022-02-12 13:51:30 +0100

    Init commit

---
+++ src/main/java/com/mindtech/mindtech/MindtechInterviewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mindtech.mindtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindtechInterviewProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindtechInterviewProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindtech/mindtech/MindtechInterviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtech.mindtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindtechInterviewProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
